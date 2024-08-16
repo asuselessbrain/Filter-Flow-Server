6e38a499a1ae03fc13381c56e396712f469120a6
asuselessbrain <shovon1023456789@gmail.com>
2024-08-17 02:31:09 +0600

    vercel.json file added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,9 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://ahmedshohagarfan:HPgOhkFFhPvUXgvv@cluster0.x6ipdw6.mongodb.net/?appName=Cluster0";
-// const uri = "mongodb://localhost:27017"
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x6ipdw6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
